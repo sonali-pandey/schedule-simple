8e9f6d903a8fd582e2d715555ebb0cd6ed1c1dc3
Sona <sonali23.1293@gmail.com>
2021-05-24 16:14:44 -0400

    color-coding time-blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,27 +5,51 @@ var containerEl = $(".container");
 // display current day
 $("#currentDay").text(now.format("dddd, MMMM Do"));
 
+// Creating an array with the hours of the day
 var dayHour = Array.from(new Array(9)).map(function (v,i){
-    var text = dayjs().hour(i+9).format("h:00 A");
-    return (text);
+    var hr12 = dayjs().hour(i+9).format("h:00 A");
+    var hr24 = dayjs().hour(i+9).format("H");
+    return {hr12,hr24};
 });
 
+// time-block: hour of the day, text, save button
 dayHour.forEach(function(hour){
     var timeBlock = $("<form>")
         .addClass("row time-block");
 
     var hourTab = $("<div>")
-        .addClass("col-2 hour")
-        .text(hour);
-
+        .addClass("hour col-2");
+    
+    var hourText = $("<p>")
+        .addClass("m-4")
+        .text(hour.hr12);
+        
     var task = $("<textarea>")
-        .addClass("col-9 description");
+        .addClass("col-9 description form-control")
+        .attr("id",hour.hr24);
     
     var saveBtn = $("<button class='saveBtn col-1'><i class='fas fa-save'></i></button>");
 
     containerEl.append(timeBlock);
     timeBlock.append(hourTab);
     timeBlock.append(task);
-    timeBlock.append(saveBtn);
-    
+    timeBlock.append(saveBtn); 
+    hourTab.append(hourText)
 });
+
+// Color Coding Time-Blocks
+$(".description").each(function(){
+    var block = parseInt($(this).attr("id"));
+    var time = parseInt(now.format("H"));
+
+    if(block===time){
+        $(this).addClass("present");
+    }
+    else if(block<time)
+    {
+        $(this).addClass("past");
+    }
+    else{
+        $(this).addClass("future");
+    }
+})
